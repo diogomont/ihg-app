251e904df08a7b2111689428a60871c572ad656e
Diogo Monteiro <diogomont@gmail.com>
2019-11-02 01:27:02 -0700

    add font loading, search field, and button

--- App.tsx
+++ App.tsx
@@ -2,11 +2,37 @@ import React from "react";
 import { SafeAreaView } from "react-native";
 import { constants } from "./constants/constants";
 import { Home } from "./screens/Home";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <SafeAreaView style={{ paddingTop: constants.androidPadding }}>
-      <Home />
-    </SafeAreaView>
-  );
+export default class App extends React.Component<{}> {
+  state = {
+    fontsLoaded: false
+  };
+
+  async componentDidMount() {
+    console.log("trying to load fonts");
+    try {
+      await Font.loadAsync({
+        Inter: require("./assets/fonts/Inter/Inter-Regular.ttf"),
+        InterBold: require("./assets/fonts/Inter/Inter-Bold.ttf"),
+        RobotoMono: require("./assets/fonts/RobotoMono/RobotoMono-Regular.ttf"),
+        RobotoMonoBold: require("./assets/fonts/RobotoMono/RobotoMono-Bold.ttf")
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    console.log("loaded fonts");
+    this.setState({ fontsLoaded: true });
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return null;
+    }
+    return (
+      <SafeAreaView style={{ paddingTop: constants.androidPadding }}>
+        <Home />
+      </SafeAreaView>
+    );
+  }
 }
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,23 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { Icon } from "../components/Icon";
 import { constants } from "../constants/constants";
 
 export const Home = () => {
   return (
-    <SafeAreaView style={{ paddingTop: constants.androidPadding }}>
-      <View style={styles.container}>
-        <Icon />
-        <Text style={{ color: "red" }}>Generate hash tags</Text>
+    <View style={styles.container}>
+      <Icon />
+      <View>
+        <Text
+          style={{
+            color: "red",
+            fontFamily: "InterBold",
+            fontSize: 16,
+            marginBottom: 10,
+            marginTop: 15
+          }}
+        >
+          Generate hash tags
+        </Text>
+        <TextInput
+          style={{
+            backgroundColor: "#EAF2FF",
+            color: "#97ABCB",
+            fontFamily: "RobotoMonoBold",
+            fontSize: 16,
+            height: 50,
+            marginBottom: 6,
+            paddingHorizontal: 12
+          }}
+          value="#kimchi #architecture #fashion"
+        />
+        <Button
+          style={{ backgroundColor: "#FF0000", borderRadius: "8px" }}
+          title="SEARCH"
+        />
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: constants.containerPadding,
-    flex: 1,
+    // margin: constants.containerPadding,
+    margin: 22,
+    // flex: 1,
     backgroundColor: "#fff"
   }
 });
